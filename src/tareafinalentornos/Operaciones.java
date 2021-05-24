f2b0dca793ab690554708b1f36cc22f10d8bae3b
riot1101 <emirem.ballon@gmail.com>
2021-05-24 18:35:23 +0200

    commit inicial

---
+++ src/tareafinalentornos/Operaciones.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareafinalentornos;
+
+/**
+ *
+ * @author CES
+ */
+public class Operaciones {
+
+    public int[] rango(int a, int b) {
+        int[] nums = new int[b - a];
+        for (int i = a; i < b; i++) {
+            nums[i] = i;
+        }
+        return nums;
+    }
+
+    public int sumRango(int a, int b) {
+        int sum = 0;
+        for (int i = a; i < b; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public boolean isPar(int a) {
+        if (a % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int[] extraerPositivos(int[] nums) {
+
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                n++;
+            }
+        }
+
+        int[] positivos = new int[n];
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                positivos[j] = nums[i];
+                j++;
+            }
+        }
+        return positivos;
+    }
+}
---
+++ test/tareafinalentornos/OperacionesTest.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareafinalentornos;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author CES
+ */
+public class OperacionesTest {
+    
+    public OperacionesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of rango method, of class Operaciones.
+     */
+    @Test
+    public void testRango() {
+        System.out.println("rango");
+        int a = 0;
+        int b = 0;
+        Operaciones instance = new Operaciones();
+        int[] expResult = null;
+        int[] result = instance.rango(a, b);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of sumRango method, of class Operaciones.
+     */
+    @Test
+    public void testSumRango() {
+        System.out.println("sumRango");
+        int a = 0;
+        int b = 0;
+        Operaciones instance = new Operaciones();
+        int expResult = 0;
+        int result = instance.sumRango(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isPar method, of class Operaciones.
+     */
+    @Test
+    public void testIsPar() {
+        System.out.println("isPar");
+        int a = 0;
+        Operaciones instance = new Operaciones();
+        boolean expResult = false;
+        boolean result = instance.isPar(a);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of extraerPositivos method, of class Operaciones.
+     */
+    @Test
+    public void testExtraerPositivos() {
+        System.out.println("extraerPositivos");
+        int[] nums = null;
+        Operaciones instance = new Operaciones();
+        int[] expResult = null;
+        int[] result = instance.extraerPositivos(nums);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
